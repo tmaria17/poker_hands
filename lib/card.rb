b895d5553d8b52691f712eacbcc87d5bd2d6011f
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-04-11 13:30:34 -0600

    Create card class.

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,5 @@
 class Card
+  attr_reader :value, :suit
   def initialize(card_data)
     @value = card_data.chars[0]
     @suit = card_data.chars[1]
--- test/card_test.rb
+++ test/card_test.rb
@@ -9,4 +9,14 @@ class CardTest < Minitest::Test
     card = Card.new("4D")
     assert_instance_of Card, card
   end
+
+  def test_it_has_a_value
+    card = Card.new("4D")
+    assert_equal card.value, "4"
+  end
+
+  def test_it_has_a_suit
+    card = Card.new("4D")
+    assert_equal card.suit, "D"
+  end
 end
