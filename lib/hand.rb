1743a8365a88d0142a397304c4a419a2265dc2a2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-04-11 15:45:36 -0600

    Create a method to hold card objects.

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,4 +1,13 @@
+require_relative 'card'
 class Hand
+  attr_reader :card_array, :cards
   def initialize(card_array)
+    @card_array = card_array
+  end
+  
+  def cards
+    @card_array.map do |card_data|
+      Card.new(card_data)
+    end
   end
 end
--- test/hand_test.rb
+++ test/hand_test.rb
@@ -9,4 +9,15 @@ class HandTest < Minitest::Test
     hand = Hand.new(["5H", "5C", "6S", "7S", "KD"])
     assert_instance_of Hand, hand
   end
+
+  def test_it_holds_a_card_array
+    hand = Hand.new(["5H", "5C", "6S", "7S", "KD"])
+    assert_equal ["5H", "5C", "6S", "7S", "KD"], hand.card_array
+  end
+
+  def test_it_has_cards_with_card_attributes
+    hand = Hand.new(["5H", "5C", "6S", "7S", "KD"])
+    assert_equal "5", hand.cards.first.value
+    assert_equal "H", hand.cards.first.suit
+  end
 end
