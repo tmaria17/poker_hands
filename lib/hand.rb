f51b162b2449aac964ddaf6ba0e0de670875c6e2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-04-11 17:29:32 -0600

    Implement code that returns true or false if there is one pair or two pairs.

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,10 +1,34 @@
 require_relative 'card'
+require 'pry'
 class Hand
-  attr_reader :card_array, :cards
   def initialize(card_array)
     @card_array = card_array
   end
   
+  def two_pair?
+    values.any? { |value| values.count(value) == 3 }
+  end
+
+  def one_pair?
+    values.any? { |value| values.count(value) == 2 }
+  end
+
+  def suites
+    suit_array = [ ]
+    cards.each do |card|
+      suit_array.push(card.suit)
+    end
+    return suit_array
+  end
+
+  def values
+    value_array = [ ]
+    cards.each do |card|
+      value_array.push(card.value)
+    end
+    return value_array
+  end
+
   def cards
     @card_array.map do |card_data|
       Card.new(card_data)
