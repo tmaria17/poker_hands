e661f0bee8cfd435f8a3ae7982e9ddcc1fd590ed
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-04-12 12:57:27 -0600

    Adds winning conditons to round class and tests.

--- lib/round.rb
+++ lib/round.rb
@@ -7,4 +7,82 @@ class Round
     @player_1 = Hand.new(card_array[0..4])
     @player_2 = Hand.new(card_array[5..9])
   end
+
+  def winner
+    if @player_1.royal_flush?
+      @player_1
+    elsif @player_2.royal_flush?
+      @player_2
+    elsif @player_1.straight_flush? && @player_1.straight_flush?
+      tie
+    elsif @player_1.straight_flush?
+      @player_1
+    elsif @player_2.straight_flush?
+      @player_2
+    elsif @player_1.four_of_a_kind? && @player_2.four_of_a_kind?
+        tie
+    elsif @player_1.four_of_a_kind?
+        @player_1
+    elsif @player_2.four_of_a_kind?
+      @player_2
+    elsif @player_1.full_house? && @player_2.full_house?
+      tie
+    elsif @player_1.full_house?
+      @player_1
+    elsif @player_2.full_house?
+      @player_2
+    elsif @player_1.flush? && @player_2.flush?
+      tie
+    elsif @player_1.flush?
+      @player_1
+    elsif @player_2.flush?
+      @player_2
+    elsif @player_1.straight? && @player_2.straight?
+      tie
+    elsif @player_1.straight?
+      @player_1
+    elsif @player_2.straight?
+      @player_2
+    elsif @player_1.three_of_a_kind? && @player_2.three_of_a_kind?
+      tie
+    elsif @player_1.three_of_a_kind?
+      @player_1
+    elsif @player_2.three_of_a_kind?
+      @player_2
+    elsif @player_1.two_pair? && @player_2.two_pair?
+      tie
+    elsif @player_1.two_pair?
+      @player_1
+    elsif @player_2.two_pair?
+      @player_2
+    elsif @player_1.one_pair? && @player_2.one_pair?
+      tie
+    elsif @player_1.one_pair?
+      @player_1
+    elsif @player_2.one_pair?
+      @player_2
+    else
+      tie
+    end
+  end
+
+  def tie
+    if @player_1.high_card > @player_2.high_card
+        @player_1
+    elsif @player_2.high_card > @player_1.high_card
+        @player_2
+    elsif @player_1.high_card == @player_2.high_card
+        @player_1.values.delete(@player_1.high_card)
+        @player_2.values.delete(@player_2.high_card)
+          tie_breaker_again
+    end
+  end
+
+  def tie_breaker_again
+    if @player_1.values.max > @player_2.vales.max
+      @player_1
+    elsif @player_2.values.max > @player_1.vales.max
+      @player_2
+    end
+  end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -7,6 +7,7 @@ require 'pry'
 class RoundTest < Minitest::Test
   def test_it_exists
     round = Round.new(["8C", "TS", "KC", "9H", "4S", "7D", "2S", "5D", "3S", "AC"])
+
     assert_instance_of Round, round
   end
 
@@ -14,5 +15,10 @@ class RoundTest < Minitest::Test
     round = Round.new(["8C", "TS", "KC", "9H", "4S", "7D", "2S", "5D", "3S", "AC"])
     assert_instance_of Hand, round.player_1
     assert_instance_of Hand, round.player_2
+  end
+
+  def test_it_returns_a_winner_even_if_there_is_a_tie
+    round = Round.new(["1C", "2C", "3C", "4C", "5C", "6C", "5C", "4C", "3C", "2C"])
+    assert_equal round.player_2, round.winner
   end
 end
